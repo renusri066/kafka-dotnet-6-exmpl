000bb11d0fb434c7cc10877134201cd4cf884096
renusri066 <renusri066@gmail.com>
2023-04-28 00:16:42 +0530

    resolve build error

--- Kafka-Producer-rs/Program.cs
+++ Kafka-Producer-rs/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Kafka_Producer_rs;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! Producer");
 ProduceMessage produceMessage = new ProduceMessage();
 produceMessage.CreateMessage().Wait();
